861ac9ae365374f10548927c5b9a4ff4eb77f8f9
Martynas Andriuškevičius <martynas7272@gmail.com>
2017-04-24 18:13:43 +0300

    Add redirect based on a given ID and add support for UTMs (#1)
    * Add redirect based on a given ID and add support for UTMs
    
    * Change redirect to 301

--- web.js
+++ web.js
@@ -1,12 +1,45 @@
 var express = require('express');
+var query = require('querystring');
+var url = require('url');
 
 var app = express.createServer(express.logger());
 
-app.get('*', function(request, response) {
-  response.redirect(process.env.NEW_BASE_URL + request.url)
+app.get('*', function (request, response) {
+
+    var id = parseInt(request.url.substr(1).split('.')[0]);
+
+    if (Number.isInteger(id)) {
+        var queryParams = {
+            rc: id
+        };
+
+        if (process.env.UTM_SOURCE) {
+            queryParams.utm_source = process.env.UTM_SOURCE;
+        } else {
+            var referringUrl = url.parse(request.get('Referrer'));
+
+            if (referringUrl.host) {
+                queryParams.utm_source = referringUrl.host;
+            }
+        }
+
+        if (process.env.UTM_CAMPAIGN) {
+            queryParams.utm_campaign = process.env.UTM_CAMPAIGN;
+        }
+
+        if (process.env.UTM_MEDIUM) {
+            queryParams.utm_medium = process.env.UTM_MEDIUM;
+        }
+
+        response.redirect(301, process.env.NEW_BASE_URL + '?' + query.stringify(queryParams));
+
+        return;
+    }
+
+    response.redirect(process.env.NEW_BASE_URL + request.url)
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+    console.log("Listening on " + port);
 });
