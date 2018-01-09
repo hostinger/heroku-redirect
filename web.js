ecb149f808ca4a56c7f94b967332ee4c5e111df8
linaslev <linas.levanas@hostinger.com>
2018-01-09 12:03:29 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var query = require('querystring');
 var url = require('url');
+var got = require('got');
 
 var app = express();
+
+function trackEvent (ga_tracking_id, category, action, label, value) {
+  const data = {
+    // Tracking ID / Property ID.
+    tid: ga_tracking_id,
+    // Event hit type.
+    t: 'pageview'
+  };
+
+  return got.post('http://www.google-analytics.com/collect', {
+    form: data
+  });
+}
 
 app.get('*', function (request, response) {
 
@@ -10,11 +24,17 @@ app.get('*', function (request, response) {
 
     var languageCode = request.headers.host.split('.')[0].toUpperCase();
     var baseUrl = process.env['NEW_BASE_URL_' + languageCode] || process.env['NEW_BASE_URL'];
-
+    var gaTrackingId = process.env['GA_TRACKING_ID_' + languageCode] || process.env['GA_TRACKING_ID']
+    
     if (id) {
         var queryParams = {
             rc: id
         };
+        
+        // Track GA Event
+        trackEvent(
+            gaTrackingId
+        ).catch(next);
 
         if (process.env.UTM_SOURCE) {
             queryParams.utm_source = process.env.UTM_SOURCE;
